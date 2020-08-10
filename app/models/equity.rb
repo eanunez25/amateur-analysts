2fe1dbb738a851840f6e16b367e360d0500eef3a
Elvin Nunez <eanunez25@gmail.com>
2020-08-09 21:20:59 -0500

    update validations for equity and price

--- app/models/equity.rb
+++ app/models/equity.rb
@@ -1,6 +1,6 @@
 class Equity < ApplicationRecord
   validates :ticker, presence: true, length: { maximum: 4 }, format: { with: /\A[A-Z]+\z/, message: "only allows capital letters" }
 
-  has_many: prices
-  has_many: users, through: :prices
+  has_many :prices
+  has_many :users, through: :prices
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,7 @@
 class Price < ApplicationRecord
   validates :buyprice, presence: true, numericality: { greater_than: 0 }
+  validates :equity_id, presence: true
+  validates :user_id, presence: true
 
   belongs_to :equity
   belongs_to :user
