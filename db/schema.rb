c55a2c901cfa2af740b71e1c34c18787638e70ea
Elvin Nunez <eanunez25@gmail.com>
2020-08-09 20:55:59 -0500

    fix type for buyprice in price model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_012935) do
+ActiveRecord::Schema.define(version: 2020_08_10_015321) do
 
   create_table "equities", force: :cascade do |t|
     t.string "ticker"
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2020_08_10_012935) do
   end
 
   create_table "prices", force: :cascade do |t|
-    t.float "buyprice"
     t.integer "equity_id"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "buyprice"
   end
 
   create_table "users", force: :cascade do |t|
