8bc42761c87e0598dd012aae99d74165acbb6903
Elvin Nunez <eanunez25@gmail.com>
2020-08-09 20:28:59 -0500

    create price model

---
+++ app/views/prices/_price.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! price, :id, :buyprice, :equity_id, :user_id, :created_at, :updated_at
+json.url price_url(price, format: :json)
---
+++ db/migrate/20200810012722_create_prices.rb
@@ -0,0 +1,11 @@
+class CreatePrices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prices do |t|
+      t.float :buyprice
+      t.integer :equity_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
