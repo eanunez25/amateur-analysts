2e1ea81a3031b0f9da7b2a0715096431f746ecbc
Elvin Nunez <eanunez25@gmail.com>
2020-08-12 18:13:09 -0500

    change login links and add mobile styling

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,20 @@
 module SessionsHelper
 
+  # logs in the given user
   def log_in(user)
     session[:user_id] = user.id 
   end
+
+  # returns the current logged-in user if any
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  # returns true if user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
