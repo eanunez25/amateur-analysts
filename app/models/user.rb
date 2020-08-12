bb6df62146040ad66d2cf80a01b56033a8f2a118
Elvin Nunez <eanunez25@gmail.com>
2020-08-11 20:09:33 -0500

    add email column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum: 5 }
+  USERNAME_REGEX = /\A^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$\z/
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 12 }, format: { with: USERNAME_REGEX }
 
   has_many :prices
   has_many :equities, through: :prices
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_015827) do
+ActiveRecord::Schema.define(version: 2020_08_12_010453) do
 
   create_table "equities", force: :cascade do |t|
     t.string "ticker"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_015827) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
