7efa5d6ad16e035336a3dc6924d5840b7d10235b
Elvin Nunez <eanunez25@gmail.com>
2020-08-12 18:32:22 -0500

    log out method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Amateur Analysts!"
       redirect_to @user
     else
