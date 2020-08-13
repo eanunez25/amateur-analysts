b726849907851520da0d1938e496c5348d62ca6b
Elvin Nunez <eanunez25@gmail.com>
2020-08-13 13:40:12 -0500

    seed db with sample users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   USERNAME_REGEX = /\A^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$\z/i
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 12 }, format: { with: USERNAME_REGEX }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 25 }, format: { with: USERNAME_REGEX }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 30 }, format: { with: EMAIL_REGEX }
 
