a1c09878a4666ac4a6ff0b813fe45fa290f88ba7
Elvin Nunez <eanunez25@gmail.com>
2020-08-13 22:01:50 -0500

    create reset password view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_231523) do
+ActiveRecord::Schema.define(version: 2020_08_14_025912) do
 
   create_table "equities", force: :cascade do |t|
     t.string "ticker"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_08_13_231523) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
