47afb769601ea2adbbc8dc9b74ac5716196afd1b
Elvin Nunez <eanunez25@gmail.com>
2020-08-13 22:14:45 -0500

    create password reset create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   before_save { self.email = email.downcase }
@@ -43,17 +43,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # downcase email to all lower case
-  def downcase_email
-    self.email = email.downcase
-  end
-
-  # creates and assigns the activation token and digest
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
-
   # returns true if the given token matches the digest
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
@@ -67,9 +56,34 @@ class User < ApplicationRecord
     update_attribute(:acticated_at, Time.zone.now)
   end
 
-  #sends an activation email
+  # sends an activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  private
+
+    # downcase email to all lower case
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
