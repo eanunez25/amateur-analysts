ebcc387423e0a95f43fb2e2ce234e3a971da72b7
Elvin Nunez <eanunez25@gmail.com>
2020-08-15 18:38:38 -0500

    create views for following and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,9 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
 
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :active_relationships
+  has_many :followers, through: :passive_relationships, source: :follower
 
-  # Returns the hash digest of the given string.
+  # Returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
           BCrypt::Engine.cost
@@ -101,7 +101,7 @@ class User < ApplicationRecord
   end
 
   # returns true if the current user is following the other user
-  def folowing?(other_user)
+  def following?(other_user)
     following.include?(other_user)
   end
 
