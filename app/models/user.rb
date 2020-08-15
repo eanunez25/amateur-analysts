9ad967a3da02ff26bce609c4e223cdaa5b6721d0
Elvin Nunez <eanunez25@gmail.com>
2020-08-15 14:26:45 -0500

    user has many followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,9 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
 
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :active_relationships
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
