a84720ff8c02a3184c7739a39b7edf9b574bdb7f
Elvin Nunez <eanunez25@gmail.com>
2020-08-15 00:24:06 -0500

    fix micropost ordering bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page]).order("created_at DESC")
+      @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
